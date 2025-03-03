9c663d1eb8f7dbb350237d8400f9078825b9f9fb
liyong0310 <474885329@qq.com>
2025-03-03 12:49:41 +0800

    connect

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+<<<<<<< HEAD
 def main():
     print("Hello World!")
 if __name__ == "__main__":
     main()
     print("again")
+=======
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print('hello world!')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+>>>>>>> 973a317 (connect)
